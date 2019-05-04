31662334aeffd33d1c63f0b337a34e33b8609ecc
aleksandrakonopka <aleksandrakonopka424@gmail.com>
2019-05-04 11:53:25 +0200

    working in one file

--- Countries/ViewController.swift
+++ Countries/ViewController.swift
@@ -11,40 +11,42 @@ import UIKit
 struct Country : Decodable
 {
     var name: String
-    //var topLevelDomain
-    //var alpha2Code
-    //var alpha3Code
-    //var callingCodes : [Int]
+    var topLevelDomain : [String]
+    var alpha2Code : String
+    var alpha3Code : String
+    var callingCodes : [String]
     var capital : String
-    //var altSpellings
+    var altSpellings : [String]
     var region : String
-    //var subregion
+    var subregion : String
     var population : Int
     var latlng : [Float]
-    //var demonym
-    //var area
-    //var gini
-    //var timezones
-    //var borders
-    var nativeName: String
-    //var numericCode
+    var demonym : String
+    var area : Double? = 0
+ //  var gini : Double
+    var timezones : [String]
+    var borders : [String]
+    var nativeName : String
+    //var numericCode : String
     //var currencies
     //var languages
-    //var translations
-    //var flag
+    //var translations : [String: String]
+    var flag : String
     //var regionBlocs
-    //var cioc
+    //var cioc : String
 }
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        print("Zaczynamy!")
         let jsonUrlString = "https://restcountries.eu/rest/v2/all"
         let url = URL(string:jsonUrlString)!
         
         URLSession.shared.dataTask(with: url) { (data, response, err) in
             do {
+                //print("weszlo")
                 guard let data = data else {return}
                 let countries = try JSONDecoder().decode([Country].self, from: data)
                 print(countries)
